de02289547c245fbc6341515dbe81cb4ceb808c9
yGit <yGit@.com>
2022-08-31 15:14:28 +0800

    master push commit

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("hello Git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
