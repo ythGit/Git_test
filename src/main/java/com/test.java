b6a8c093cbb884abcb55b7346ad43e49d9e29e8a
yGit <yGit@.com>
2022-08-31 14:50:59 +0800

    master commit2

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("hello Git2");
         System.out.println("hello Git3");
         System.out.println("hello Git4");
+        System.out.println("master test");
     }
 }
